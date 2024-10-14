2b28044c400015170a6209bd871ae81c15e64cdb
yd1107 <tomoko1107.ky413@gmail.com>
2024-10-14 15:43:24 +0900

    ガイド5レビュー依頼

--- kadai_002/accounts/urls.py
+++ kadai_002/accounts/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+
+
+urlpatterns = [
+  path("user-detail/<int:pk>/", views.UserDetailView.as_view(), name="user_detail"),
+  path("user-update/<int:pk>/", views.UserUpdateView.as_view(), name="user_update"),
+]
--- kadai_002/accounts/views.py
+++ kadai_002/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from . import forms
+from . import models
 
 # Create your views here.
+class UserDetailView(generic.DetailView):
+    model = models.CustomUser
+    template_name = 'user/user_detail.html'
+    
+
+class UserUpdateView(generic.UpdateView):
+    model = models.CustomUser
+    template_name = 'user/user_update.html'
+    form_class = forms.UserUpdateForm
+
+    def get_success_url(self):
+      pk = self.kwargs['pk']
+      return reverse_lazy('user_detail', kwargs={'pk': pk})
+
+    def form_valid(self, form):
+      return super().form_valid(form)
+      
+    def form_invalid(self, form):
+      return super().form_invalid(form)
